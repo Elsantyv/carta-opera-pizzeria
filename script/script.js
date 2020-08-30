4059423199f4d5e2137fb02130290c68a7b9c355
ElSanty <vanegasdesarrollo@gmail.com>
2020-08-29 22:21:29 -0500

    Arreglos

--- script/script.js
+++ script/script.js
@@ -12,7 +12,27 @@ document.querySelectorAll(".modal-container").forEach(el => {
 
     el.addEventListener("click", function(ev) {
 
-        this.parentNode.classList.remove("active");
+        this.classList.remove("active");
+        console.log("Sali");
+    })
+
+});
+
+document.querySelectorAll(".modal-container img").forEach(el => {
+
+    el.addEventListener("click", function(ev) {
+        ev.stopPropagation();
+        this.parentNode.classList.add("active");
+        console.log("Entre");
+    })
+
+});
+
+document.querySelectorAll(".modal-container").forEach(el => {
+
+    el.addEventListener("click", function(ev) {
+
+        this.classList.remove("active");
         console.log("Sali");
     })
 
